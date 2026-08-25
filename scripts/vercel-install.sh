a3d42f43d5bcc1a5612ac5e08c90e837dc64e49f
aniketdvnks <aniket@dvnks.com>
2026-08-25 21:34:40 +0530

    fix: use npm for vercel dependency install

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -13,4 +13,4 @@ git config --global --add url."${token_url}".insteadOf "git+ssh://git@github.com
 git config --global --add url."${token_url}".insteadOf "ssh://git@github.com/"
 git config --global --add url."${token_url}".insteadOf "git@github.com:"
 
-pnpm install --no-frozen-lockfile
+npm install
